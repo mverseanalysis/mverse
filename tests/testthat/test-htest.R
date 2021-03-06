7076dbb222aaef18c8521c7401cd23ff11aeaf2a
yurina! <lihd1003@hotmail.com>
2021-03-06 16:50:31 -0500

    add test for ttest_mverse

---
+++ tests/testthat/test-htest.R
@@ -0,0 +1,19 @@
+test_that("ttest_mverse() computes t.test.", {
+  mydf <- data.frame(
+    x=rnorm(100, 0, 2),
+    y=rnorm(100)
+  )
+  mv <- create_multiverse(mydf)
+  mbranch <- mutate_branch(x + y, x - y, x * y, name = "m")
+  mv %>%
+    add_mutate_branch(mbranch)
+  ttest_mverse(mv, 'x')
+  fitmverse <- (multiverse::extract_variables(mv, htest) %>% pull(htest))[[1]]
+  fitmanual <- t.test(mydf$x)
+  expect_identical(fitmverse$statistic, fitmanual$statistic)
+  ttest_mverse(mv, 'x', 'm')
+  fitmverse <- (multiverse::extract_variables(mv, htest) %>% pull(htest))[[1]]
+  fitmanual <- t.test(mydf$x, mydf$x + mydf$y)
+  expect_identical(fitmverse$statistic, fitmanual$statistic)
+
+})
