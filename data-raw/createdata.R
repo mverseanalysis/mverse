80f5cf63c6e45719e2546dfe241afc535263fc1a
Michael Jongho Moon <michael.jongho.moon@gmail.com>
2022-05-03 16:56:10 -0400

    recompress data usign bzip2 and move tidyverse and scales to Suggests (only used in vignettes)

--- data-raw/createdata.R
+++ data-raw/createdata.R
@@ -4,8 +4,11 @@ drops <- c("photoID", "Alpha_3")
 soccer <- soccer[, !(names(soccer) %in% drops)]
 soccer$player <- stringi::stri_trans_general(soccer$player, "latin-ascii")
 soccer$club <- stringi::stri_trans_general(soccer$club, "latin-ascii")
-usethis::use_data(soccer, compress = "gzip", overwrite = TRUE)
-
+usethis::use_data(soccer, overwrite = TRUE)
+tools::checkRdaFiles("data/soccer.rda")
+tools::resaveRdaFiles("data/soccer.rda", compress = "bzip2")
 #' Read and write hurricane data.
 data("hurricane", package = "multiverse")
-usethis::use_data(hurricane, compress = "gzip", overwrite = TRUE)
+usethis::use_data(hurricane, overwrite = TRUE)
+tools::checkRdaFiles("data/hurricane.rda")
+tools::resaveRdaFiles("data/hurricane.rda", compress = "bzip2")
