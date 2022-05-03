c51cf3ec97be5da1b13d995096c4986a90da031b
Michael Jongho Moon <michael.jongho.moon@gmail.com>
2022-05-03 16:38:02 -0400

    compress data

--- data-raw/createdata.R
+++ data-raw/createdata.R
@@ -4,4 +4,8 @@ drops <- c("photoID", "Alpha_3")
 soccer <- soccer[, !(names(soccer) %in% drops)]
 soccer$player <- stringi::stri_trans_general(soccer$player, "latin-ascii")
 soccer$club <- stringi::stri_trans_general(soccer$club, "latin-ascii")
-save(soccer, file = "data/soccer.RData")
+usethis::use_data(soccer, compress = "gzip", overwrite = TRUE)
+
+#' Read and write hurricane data.
+data("hurricane", package = "multiverse")
+usethis::use_data(hurricane, compress = "gzip", overwrite = TRUE)
